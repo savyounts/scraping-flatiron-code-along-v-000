f980a3ee85fc688299ffd0c480b2769856f8ee90
Sav Younts <savyounts@gmail.com>
2018-08-12 22:28:56 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,9 +11,9 @@ class Scraper
     
     doc.css(".post").each do |post|
       course = Course.new 
-      course.title = doc.css(".post").css.("h2").text  
-      course.schedule = doc.css(".post").css(".date").text
-      course.description = doc.css(".post").css("p").text
+      course.title = post.css.("h2").text  
+      course.schedule = post.css(".post").css(".date").text
+      course.description = post.css(".post").css("p").text
      
     end
    
